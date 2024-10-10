d28fff038ef32a361adef84c56b0ca210fe2b068
spec2r3 <allanjonesbiju@gmail.com>
2024-10-10 16:37:30 +0100

    Configured svetle frontend

--- backend/main.go
+++ backend/main.go
@@ -11,7 +11,22 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
+	enableCORS(server)
+
 	routes.RegisterRoutes(server)
 
-	server.Run(":8080") //localhost:8080
+	server.Run(":8081") //localhost:8080
+}
+
+func enableCORS(router *gin.Engine) {
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 }
